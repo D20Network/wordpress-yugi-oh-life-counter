48e31e572adc729d9b21201f036423012f1a0ba4
Void, Corp <106594198+D20Network@users.noreply.github.com>
2024-07-08 10:14:08 -0600

    Update yugioh-life-counter.js

--- yugioh-life-counter.js
+++ yugioh-life-counter.js
@@ -1,19 +1,33 @@
 jQuery(document).ready(function($) {
-    $('.lp-btn').click(function() {
-        var change = parseInt($(this).data('lp-change'));
+    console.log("Document ready. Initializing event handlers.");
+
+    $('.lp-btn').off('click').on('click', function() {
+        var change = parseInt($(this).data('lp-change'), 10);
         var $lpDisplay = $(this).siblings('.lp-display');
-        var currentLP = parseInt($lpDisplay.data('lp'));
+        var currentLP = parseInt($lpDisplay.attr('data-lp'), 10);
+
+        console.log("Button clicked. Change: ", change);
+        console.log("Current LP: ", currentLP);
+
         var newLP = currentLP + change;
-        $lpDisplay.data('lp', newLP).text('LP: ' + newLP);
+
+        console.log("New LP: ", newLP);
+
+        // Update the data attribute and text
+        $lpDisplay.attr('data-lp', newLP);
+        $lpDisplay.text('LP: ' + newLP);
     });
 
-    $('#roll-dice').click(function() {
+    $('#roll-dice').off('click').on('click', function() {
         var result = Math.floor(Math.random() * 6) + 1;
+        console.log("Dice roll result: ", result);
         $('#dice-result').text('Dice Result: ' + result);
     });
 
-    $('#flip-coin').click(function() {
+    $('#flip-coin').off('click').on('click', function() {
         var result = Math.random() < 0.5 ? 'Heads' : 'Tails';
+        console.log("Coin flip result: ", result);
         $('#coin-result').text('Coin Result: ' + result);
     });
 });
+
